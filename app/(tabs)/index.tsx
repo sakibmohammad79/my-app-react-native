830a5c085ac94ebeb4ca681ffee20635364ff65c
sakib <mohammadsakib7679@gmail.com>
2024-08-19 16:51:26 +0600

    style home page upper part

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,77 +1,75 @@
-import { Image, StyleSheet, Platform, View, Text } from "react-native";
+import {
+  Image,
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  ImageBackground,
+} from "react-native";
 
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import {
+  Inter_400Regular,
+  Inter_700Bold,
+  Inter_900Black,
+  useFonts,
+} from "@expo-google-fonts/inter";
 
 export default function HomeScreen() {
+  const [fonstLoaded] = useFonts({
+    "inter-reguler": Inter_400Regular,
+    "inter-bold": Inter_700Bold,
+  });
+  if (!fonstLoaded) {
+    return <ActivityIndicator />;
+  }
   return (
-    // <ParallaxScrollView
-    //   headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-    //   headerImage={
-    //     <Image
-    //       source={require("@/assets/images/partial-react-logo.png")}
-    //       style={styles.reactLogo}
-    //     />
-    //   }
-    // >
-    //   <ThemedView style={styles.titleContainer}>
-    //     <ThemedText type="title">Welcome!!</ThemedText>
-    //     <HelloWave />
-    //   </ThemedView>
-    //   <ThemedView style={styles.stepContainer}>
-    //     <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-    //     <ThemedText>
-    //       Edit{" "}
-    //       <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-    //       to see changes. Press{" "}
-    //       <ThemedText type="defaultSemiBold">
-    //         {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-    //       </ThemedText>{" "}
-    //       to open developer tools.
-    //     </ThemedText>
-    //   </ThemedView>
-    //   <ThemedView style={styles.stepContainer}>
-    //     <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-    //     <ThemedText>
-    //       Tap the Explore tab to learn more about what's included in this
-    //       starter app.
-    //     </ThemedText>
-    //   </ThemedView>
-    //   <ThemedView style={styles.stepContainer}>
-    //     <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-    //     <ThemedText>
-    //       When you're ready, run{" "}
-    //       <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-    //       to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-    //       directory. This will move the current{" "}
-    //       <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-    //       <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-    //     </ThemedText>
-    //   </ThemedView>
-    // </ParallaxScrollView>
-    <View style={{ backgroundColor: "white", flex: 1 }}>
-      <Text>Hello World!</Text>
-    </View>
+    <ImageBackground
+      source={require("../../assets/images/clock-app-bg.png")}
+      style={{ flex: 1 }}
+    >
+      <View
+        style={{
+          paddingHorizontal: 26,
+          marginTop: 42,
+          justifyContent: "space-between",
+          flex: 1,
+        }}
+      >
+        {/* upper view */}
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ flex: 1 }}>
+            <Text
+              style={{
+                fontFamily: "inter-reguler",
+                fontSize: 12,
+                color: "white",
+              }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+              fugit delectus repudiandae modi, ipsam ad dolorum. Ipsam unde
+              quaerat, doloremque saepe dolor similique? Soluta modi, corporis
+              quae cupiditate autem eum.
+            </Text>
+            <Text
+              style={{
+                fontFamily: "inter-bold",
+                fontSize: 12,
+                color: "white",
+                marginTop: 8,
+              }}
+            >
+              Lorem ipsum
+            </Text>
+          </View>
+
+          <Image source={require("../../assets/images/clock-refresh.png")} />
+        </View>
+
+        {/* bottom view */}
+        <View></View>
+      </View>
+    </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
+const styles = StyleSheet.create({});
